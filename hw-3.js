7b4b955b41ecc9e06002cb9691d97589ca0779cc
Griannagri <an.grishaewa@yandex.ru>
2024-08-19 13:30:17 +0300

    Исправления

--- hw-3.js
+++ hw-3.js
@@ -1,82 +1,82 @@
-// // Lesson 1 
-// let password = '123';
-// let answer = prompt('Введите пароль');
+// Lesson 1 
+let password = '123';
+let answer = prompt('Введите пароль');
 
-// if (password === answer) {
-//   alert('Пароль введен верно');
-// } else {
-//   alert('Пароль введен неправильно');
-// }
+if (password === answer) {
+  alert('Пароль введен верно');
+} else {
+  alert('Пароль введен неправильно');
+}
 
 // Lesson 2
-// let c = prompt ('Введите число');
+let c = prompt ('Введите число');
 
-// if (0 < c && c < 10) {
-//   alert('Верно');
-// } else {
-//   alert('Неверно');
-// }
+if (0 < c && c < 10) {
+  alert('Верно');
+} else {
+  alert('Неверно');
+}
 
 // Lesson 3
-// let d=5
-// let e=3
-// if (d > 100 || e > 100) {
-// alert ('Верно')
-// } else { 
-// alert ('Неверно');
-// }
+let d=5
+let e=3
+if (d > 100 || e > 100) {
+alert ('Верно')
+} else { 
+alert ('Неверно');
+}
 
 // Lesson 4
-// let a='2'
-// let b='3'
-// a= Number(a);
-// b= Number(b);
-// alert (a + b);
+let a='2'
+let b='3'
+a= Number(a);
+b= Number(b);
+alert (a + b);
 
 // Lesson 5
-// const monthNumber = Number (prompt('Введите число'));
+const monthNumber = Number (prompt('Введите число'));
 
-// switch (monthNumber) {
-//     case '1':
-//     case '2':
-//     case '12': 
-//         console.log('Зима');
-//     case '3':
-//     case '4':
-//     case '5': 
-//         console.log('Весна');
-//     case '6':
-//     case '7':
-//     case '8': 
-//         console.log('Лето');
-//     case '9':
-//     case '10':
-//     case '11': 
-//         console.log('Осень');
-// break;
-// default:
-//     console.log('Ввели что-то не то');
-// }
+switch (monthNumber) {
+    case '1':
+    case '2':
+    case '12': 
+        console.log('Зима');
+    case '3':
+    case '4':
+    case '5': 
+        console.log('Весна');
+    case '6':
+    case '7':
+    case '8': 
+        console.log('Лето');
+    case '9':
+    case '10':
+    case '11': 
+        console.log('Осень');
+break;
+default:
+    console.log('Ввели что-то не то');
+}
 
 // Lesson 6
 
 // Lesson 7
-// let number = prompt ('Пожалуйста, введите любое число');
-// let numberr = NaN
-// if (number % 2 === 0) {
-//     alert("Число четное");
-//   } else {
-//     alert("Число нечетное");
-//   }
+let number = prompt ('Пожалуйста, введите любое число');
+let numberr = NaN
+if (number % 2 === 0) {
+    alert("Число четное");
+  } else {
+    alert("Число нечетное");
+  }
 
 // Lesson 8
 
-// const clientOS = 0;
-// if (clientOS === 0) {
-//     console.log ("Установите версию приложения для iOS по ссылке");
-// } else if (clientOS === 1) {
-//     console.log ("Установите версию приложения для Android по ссылке");
-// }
+const clientOS = 0;
+if (clientOS === 0) {
+    console.log ("Установите версию приложения для iOS по ссылке");
+} else if (clientOS === 1) {
+    console.log ("Установите версию приложения для Android по ссылке");
+}
 
 // Lesson 9
 
